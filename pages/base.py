e5c296256d5b60907664bfe0c87a4b9e1e78ad30
darkproot <woumojohnny@gmail.com>
2025-03-26 12:14:45 +0100

    Page d'acceuil terminer

--- pages/base.py
+++ pages/base.py
@@ -1,5 +1,5 @@
-from flet import Container, Column, Page, Row, Text, MainAxisAlignment, padding
-from flet import Animation, ControlEvent, Border, BorderSide, icons, Icon
+from flet import Container, Column, Page, Row, Text, MainAxisAlignment, padding, colors, CrossAxisAlignment
+from flet import Animation, ControlEvent, Border, BorderSide, icons, Icon, Image, TextStyle, Offset, BoxShadow
 from modules.color import DEEP_BLUE, BLUE, BG
 from pages import chapitres
 from modules.fonctions import pourcentage
@@ -109,5 +109,66 @@ class Display(Container):
             border_radius=10,
             expand=True,
             height=page.window.height - 70,
-        ),
-        self.content = Column([Row([Text("PlaceHolder", text_align='center', expand=True, color=DEEP_BLUE, weight='bold', size=50)])], alignment=MainAxisAlignment.CENTER)
+            padding=padding.only(pourcentage(page.window.width, 10), 15, pourcentage(page.window.width, 10)),
+        )
+        self.content = Column(
+            controls=[
+                Container(
+                    content=Column([
+                        Row([Logo(r"assets\logo.png")], expand=True, alignment=MainAxisAlignment.CENTER),
+                        Row([Name("C Learning")], expand=True, alignment=MainAxisAlignment.CENTER),
+                    ], spacing=40)
+                )
+            ],
+            alignment=MainAxisAlignment.CENTER,
+        )
+
+class Logo(Container):
+    def __init__(self, path: str):
+        super().__init__(
+            blur=5,
+            border_radius=150,
+            bgcolor=colors.with_opacity(.2, DEEP_BLUE),
+        )
+        self.data = 'a propos'
+        self.path = path
+        self.animate = Animation(200, 'ease')
+        self.content = Image(
+            self.path, 
+            border_radius=20,
+            animate_scale=self.animate,
+            height=250,
+            width=250,
+        )
+        self.on_hover = self.hover
+        self.on_click = change_page
+
+    def hover(self, e: ControlEvent):
+        page: Page = e.page
+        if e.data == 'true':
+            self.content.scale = 1.15
+        else:
+            self.content.scale = 1
+        page.update()
+
+class Name(Container):
+    def __init__(self, name: str):
+        super().__init__()
+        self.name = name
+        self.expand = True
+        self.text_style = TextStyle(
+            size=70,
+            weight='bold',
+            color=colors.with_opacity(.5, BLUE),
+            font_family='texte',
+            shadow=BoxShadow(0, 1, 'black12', Offset(3, 3)),
+        )
+        self.content = Column(
+            controls=[
+                Text("Bienvenue sur", font_family='texte', color='black', weight='bold', size=30),
+                Text(self.name, style=self.text_style, expand=True),
+            ],
+            spacing=0,
+            expand=True,
+            horizontal_alignment=CrossAxisAlignment.CENTER,
+        )
